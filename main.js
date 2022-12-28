347db3de98de22e729cef2538da72558e1e8b74b
fakhad <dkmc@gmail.com>
2022-12-28 23:06:23 +0500

    save

---
+++ main.js
@@ -0,0 +1,52 @@
+var time = +prompt ('Введите время');
+
+switch (time) {
+    case 1:
+    alert('1часов ночи');
+    case 2:
+    alert('2часа ночи');
+    case 3:
+    alert('3часов ночи');
+    case 4:
+    alert('4часов ночи');
+    case 5:
+    alert('5часов утра');
+    case 6:
+    alert('6часов утра');
+    case 7:
+    alert('7часов утра');
+    case 8:
+    alert('8часов утра');
+    case 9:
+    alert('9часов утра');
+    case 10:
+    alert('10часов дня');
+    case 11:
+    alert('11часов дня');
+    case 12:
+    alert('12часов дня');
+    case 31:
+    alert('1 час дня');
+    case 14:
+    alert('2 часа дня');
+    case 15:
+    alert('3 час дня');
+    case 16:
+    alert('4 часа дня');
+    case 17:
+    alert('5 часов вечера');
+    case 18:
+    alert('6 часов вечера');
+    case 19:
+    alert('7часов вечера');
+    case 20:
+    alert('8 час вечера');
+    case 21:
+    alert('9 часа вечера');
+    case 22:
+    alert('10 часов ночи');
+    case 23:
+    alert('11 часов ночи');
+    case 00:
+    alert(' полночь');
+}
